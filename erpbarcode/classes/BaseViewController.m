eb941f05533cddc9200ef5d2884919e1c3168e4b
matsua <matsua12@gmail.com>
2018-12-07 21:03:59 +0900

    베이스 수정

--- erpbarcode/classes/BaseViewController.m
+++ erpbarcode/classes/BaseViewController.m
@@ -23,16 +23,12 @@
 @property(nonatomic,strong) IBOutlet UIView *locCodeView;
 @property(nonatomic,strong) IBOutlet UIView *facCodeView;
 @property(nonatomic,strong) IBOutlet UITextField *locCode;
-@property(nonatomic,strong) NSString *strLocBarCode;
 @property(nonatomic,strong) IBOutlet UITextField *facCode;
-@property(nonatomic,strong) NSString *strFacBarCode;
 @property(nonatomic,strong) NSString *bsnNo;
 @property(nonatomic,assign) NSInteger nSelected;
 @property(nonatomic,strong) IBOutlet UIWebView *resultWebView;
 @property(nonatomic,assign) __block BOOL isOperationFinished;
-@property(nonatomic,assign) BOOL isDataSaved;
 @property(nonatomic,strong) NSString* JOB_GUBUN;
-@property(nonatomic,assign) BOOL isOffLine;
 @property(nonatomic,assign) int scanBtnTag;
 
 @end
@@ -48,17 +44,13 @@
 @synthesize locCodeView;
 @synthesize facCodeView;
 @synthesize locCode;
-@synthesize strLocBarCode;
 @synthesize facCode;
-@synthesize strFacBarCode;
 @synthesize bsnNo;
 @synthesize bsnGb;
 @synthesize nSelected;
 @synthesize resultWebView;
 @synthesize isOperationFinished;
-@synthesize isDataSaved;
 @synthesize JOB_GUBUN;
-@synthesize isOffLine;
 @synthesize scanBtnTag;
 
 #pragma mark - View LifeCycle
@@ -85,38 +77,8 @@
         bsnGb = @"OE";
     }
     
-    isOffLine = [[Util udObjectForKey:@"USER_OFFLINE"] boolValue];
-    
-    [self workDataInit];
-    
     [self makeDummyInputViewForTextField];
-    
     [self layoutChangeSubview];
-    
-    if ([[Util udObjectForKey:USER_WORK_MODE] isEqualToString:@"N"])
-    {
-//        dbWorkDic = [NSMutableDictionary dictionary];
-    }else{
-        [self performSelectorOnMainThread:@selector(processWorkData) withObject:nil waitUntilDone:NO];
-    }
-}
-
-- (void) workDataInit
-{
-    isDataSaved = NO;
-    
-    //작업관리 초기화
-    workDic = [NSMutableDictionary dictionary];
-    taskList = [NSMutableArray array];
-    
-    [workDic setObject:[WorkUtil getWorkCode:JOB_GUBUN] forKey:@"WORK_CD"];
-    [workDic setObject:@"N" forKey:@"TRANSACT_YN"]; //미전송
-    
-    //offline 여부
-    if (isOffLine)
-        [workDic setObject:@"Y" forKey:@"OFFLINE"];
-    else
-        [workDic setObject:@"N" forKey:@"OFFLINE"];
     
 }
 
@@ -134,85 +96,6 @@
     }
 }
 
-- (IBAction) touchSaveBtn:(id)sender
-{
-    if([self saveToWorkDB]){
-        NSString* message = @"저장하였습니다.";
-        isDataSaved = YES;
-        [self showMessage:message tag:-1 title1:@"닫기" title2:nil];
-    }
-}
-
-#pragma mark - DB Method
-
-- (void)processWorkData
-{
-    if (dbWorkDic.count){
-        NSData* taskData = [dbWorkDic objectForKey:@"TASK"];
-        if (taskData.bytes > 0){
-            fetchTaskList = [NSKeyedUnarchiver unarchiveObjectWithData:taskData];
-            NSLog(@"fetchTaskList [%@]",fetchTaskList);
-        }
-        
-        if (fetchTaskList.count)
-            [self waitForGCD];
-    }
-}
-
-- (BOOL)saveToWorkDB
-{
-    NSString* workId = @"";
-    
-    if ([dbWorkDic count])
-        workId = [NSString stringWithFormat:@"%@", [dbWorkDic objectForKey:@"ID"]];
-    
-    NSMutableDictionary* workData = [NSMutableDictionary dictionary];
-    
-    [workData setObject:workDic forKey:@"WORKDIC"];
-    [workData setObject:taskList forKey:@"TASKLIST"];
-    
-    BOOL retValue = [[DBManager sharedInstance] saveWorkData:workData ToWorkDBWithId:workId];
-    
-    if(![workId length]){
-        workId = [NSString stringWithFormat:@"%d", [[DBManager sharedInstance] countSelectQuery:SELECT_LAST_ID_FROM_WORK_INFO]];
-    }
-    [workData setObject:workId forKey:@"ID"];
-    
-    dbWorkDic = [workData copy];
-    
-    return retValue;
-}
-
--(void)waitForGCD
-{
-    for (NSDictionary* dic in fetchTaskList) {
-        NSString* task = [dic objectForKey:@"TASK"];
-        NSString* value = [dic objectForKey:@"VALUE"];
-        
-        // 작업이 완료될 때까지 다음 TASK진행을 기다려준다.  이때 작업의 완료 여부를 결정하는 Bool 변수
-        isOperationFinished = NO;
-        
-        // TASK에 따른 작업 실행
-        if ([task isEqualToString:@"L"]) //위치
-        {
-            locCode.text = strLocBarCode = value;
-            isOperationFinished = YES;
-        }
-        else if ([task isEqualToString:@"F"]) //설비바코드
-        {
-            facCode.text = strFacBarCode = value;
-            isOperationFinished = YES;
-        }
-        else
-            isOperationFinished = YES;
-        
-        // 현재 작업이 완료될때까지 기다린다.
-        while (!isOperationFinished) {
-            [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:10]];
-        }
-    }
-}
-
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
@@ -222,35 +105,20 @@
 - (BOOL) processShouldReturn:(NSString*)barcode tag:(NSInteger)tag
 {
     if (tag == 100){ //위치바코드
-        strLocBarCode = barcode;
-        
         if(barcode.length != 11 && barcode.length != 14 && barcode.length != 17 && barcode.length != 21){
             [self showMessage:@"처리할 수 없는 위치바코드입니다." tag:-1 title1:@"닫기" title2:nil isError:YES];
-            locCode.text = strLocBarCode = @"";
+            locCode.text = @"";
             [locCode becomeFirstResponder];
             return YES;
         }
-        
-        //working task add
-        NSMutableDictionary* taskDic = [NSMutableDictionary dictionary];
-        [taskDic setObject:@"L" forKey:@"TASK"];
-        [taskDic setObject:strLocBarCode forKey:@"VALUE"];
-        [taskList addObject:taskDic];
     }
     else if (tag == 200){ //200 설비 바코드
-        strFacBarCode = barcode;
-        
         if (barcode.length < 16 || barcode.length > 18){
             [self showMessage:@"처리할 수 없는 설비바코드입니다." tag:-1 title1:@"닫기" title2:nil isError:YES];
-            facCode.text = strFacBarCode = @"";
+            facCode.text = @"";
             [facCode becomeFirstResponder];
             return YES;
         }
-        
-        NSMutableDictionary* taskDic = [NSMutableDictionary dictionary];
-        [taskDic setObject:@"F" forKey:@"TASK"];
-        [taskDic setObject:strFacBarCode forKey:@"VALUE"];
-        [taskList addObject:taskDic];
     }
     return YES;
 }
@@ -312,11 +180,10 @@
 }
 
 -(IBAction)requestBtn:(id)sender{
-    //TEST CODE : matsua
-    locCode.text = strLocBarCode = @"P10064962";
-    facCode.text = strFacBarCode = @"001Z00345300002812";
-    
     NSString *JOB_GUBUN = [Util udObjectForKey:USER_WORK_NAME];
+    
+    facCode.text = @"001Z00358100000917";
+//    locCode.text = @"P10028416";
     
     if([JOB_GUBUN hasPrefix:@"신규등록"] || [JOB_GUBUN hasPrefix:@"관리자변경"] || [JOB_GUBUN hasPrefix:@"재물조사"] ||
        [JOB_GUBUN hasPrefix:@"납품확인"] || [JOB_GUBUN hasPrefix:@"대여등록"] || [JOB_GUBUN hasPrefix:@"대여반납"]){
@@ -335,11 +202,11 @@
     
     
     if([JOB_GUBUN hasPrefix:@"신규등록"] || [JOB_GUBUN hasPrefix:@"관리자변경"] || [JOB_GUBUN hasPrefix:@"재물조사"] ||
-       [JOB_GUBUN hasPrefix:@"납품확인"] || [JOB_GUBUN hasPrefix:@"대여등록"] || [JOB_GUBUN hasPrefix:@"대여반납"]){
+       [JOB_GUBUN hasPrefix:@"납품확인"] || [JOB_GUBUN hasPrefix:@"대여등록"] || [JOB_GUBUN hasPrefix:@"대여반납"] || [JOB_GUBUN hasPrefix:@"불용요청"]){
         [self requestManagement];
     }
     
-    if([JOB_GUBUN hasPrefix:@"연식조회"] || [JOB_GUBUN hasPrefix:@"비품연식조회"] || [JOB_GUBUN hasPrefix:@"불용요청"]){
+    if([JOB_GUBUN hasPrefix:@"연식조회"] || [JOB_GUBUN hasPrefix:@"비품연식조회"]){
         [self requestItemSearch];
     }
 }
@@ -352,15 +219,20 @@
     requestMgr.delegate = self;
     requestMgr.reqKind = REQUEST_BASE_MANAGEMENT;
     
+    NSDictionary *userinfo = [Util udObjectForKey:USER_INFO];
     NSMutableDictionary *paramDic = [[NSMutableDictionary alloc] init];
-    [paramDic setObject:bsnNo forKey:@"COM"];                   //업무번호
-    [paramDic setObject:strFacBarCode forKey:@"BCID"];  //설비바코드
-    [paramDic setObject:strLocBarCode forKey:@"SDID"];  //위치바코드
+    
+    [paramDic setObject:bsnNo forKey:@"com"];                   //업무번호
+    [paramDic setObject:facCode.text forKey:@"bcId"];   //설비바코드
+    if(locCode.text){
+        [paramDic setObject:locCode.text forKey:@"sdId"];   //위치바코드
+    }
+    
+    [paramDic setObject:bsnGb forKey:@"facType"];                  //OA, OE 구분
+    [paramDic setObject:[userinfo objectForKey:@"userId"] forKey:@"userId"];                  //사용자아이디
     
     NSDictionary* bodyDic = [Util singleMessageBody:paramDic];
-    
     NSDictionary* rootDic  = [Util defaultMessage:[Util defaultHeader] body:bodyDic];
-    
     [requestMgr asychronousConnectToServer:API_BASE_MANAGEMENT withData:rootDic];
 }
 
@@ -371,15 +243,20 @@
     requestMgr.delegate = self;
     requestMgr.reqKind = REQUEST_BASE_ITEM_SEARCH;
     
+    NSDictionary *userinfo = [Util udObjectForKey:USER_INFO];
     NSMutableDictionary *paramDic = [[NSMutableDictionary alloc] init];
-    [paramDic setObject:bsnNo forKey:@"COM"];                   //업무번호
-    [paramDic setObject:strFacBarCode forKey:@"BCID"];  //설비바코드
     
-    NSDictionary* bodyDic = [Util singleMessageBody:paramDic];
+    [paramDic setObject:bsnNo forKey:@"com"];                   //업무번호
+    [paramDic setObject:facCode.text forKey:@"bcId"];  //설비바코드
     
-    NSDictionary* rootDic  = [Util defaultMessage:[Util defaultHeader] body:bodyDic];
+    [paramDic setObject:bsnGb forKey:@"facType"];                  //OA, OE 구분
+    [paramDic setObject:[userinfo objectForKey:@"userId"] forKey:@"userId"];                  //사용자아이디
     
-    [requestMgr asychronousConnectToServer:API_BASE_ITEM_SEARCH withData:rootDic];
+//    NSDictionary* bodyDic = [Util singleMessageBody:paramDic];
+//    NSDictionary* rootDic  = [Util defaultMessage:[Util defaultHeader] body:bodyDic];
+//    [requestMgr asychronousConnectToServer:API_BASE_ITEM_SEARCH withData:rootDic];
+    [requestMgr asychronousConnectToServer:API_BASE_ITEM_SEARCH withData:paramDic];
+    
 }
 
 #pragma IProcessRequest delegate -- call by ERPRequestManager
@@ -403,19 +280,6 @@
         NSString* message = [headerDic objectForKey:@"detail"];
         
         message = [message stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-        // 전송한 후에는 작업관리에 그 결과를 저장한다.  실패시 에러나 워닝을 저장한다.
-        if (pid == REQUEST_SEND){
-            if (status == 0)
-                [workDic setObject:@"E" forKey:@"TRANSACT_YN"];
-            else if (status == 2)
-                [workDic setObject:@"W" forKey:@"TRANSACT_YN"];
-            [workDic setObject:message forKey:@"TRANSACT_MSG"];
-            [self saveToWorkDB];
-            
-            // DB에 작업관리를 저장했으므로 본 화면에서 백버튼으로 나가도 문제 없음을 의미한다.
-            isDataSaved = YES;
-        }
-        
         // 실패 시 필요한 메시지를 뿌려주고, 적당한 처리를 한다.
         [self processFailRequest:pid Message:message Status:status];
         
@@ -450,20 +314,26 @@
     bsnNo = @"";
     NSString *JOB_GUBUN = [Util udObjectForKey:USER_WORK_NAME];
     
-    if([JOB_GUBUN isEqualToString:@"불용요청[OA]"] || [JOB_GUBUN isEqualToString:@"불용요청[OE]"] || [JOB_GUBUN isEqualToString:@"비품연식조회[OE]"] || [JOB_GUBUN isEqualToString:@"연식조회[OA]"]){
+    if([JOB_GUBUN rangeOfString:@"신규등록"].location != NSNotFound)
+        bsnNo = @"0501";
+    else if([JOB_GUBUN rangeOfString:@"관리자변경"].location != NSNotFound)
+        bsnNo = @"0504";
+    else if([JOB_GUBUN rangeOfString:@"재물조사"].location != NSNotFound)
+        bsnNo = @"0601";
+    else if([JOB_GUBUN rangeOfString:@"불용요청"].location != NSNotFound){
+         bsnNo = @"0505";
         locCodeView.hidden = YES;
     }
-    
-    if([JOB_GUBUN isEqualToString:@"신규등록[OA]"] || [JOB_GUBUN isEqualToString:@"신규등록[OE]"]) bsnNo = @"0501";
-    else if([JOB_GUBUN isEqualToString:@"관리자변경[OA]"] || [JOB_GUBUN isEqualToString:@"관리자변경[OE]"]) bsnNo = @"0504";
-    else if([JOB_GUBUN isEqualToString:@"재물조사[OA]"] || [JOB_GUBUN isEqualToString:@"재물조사[OE]"]) bsnNo = @"0601";
-    else if([JOB_GUBUN isEqualToString:@"불용요청[OA]"] || [JOB_GUBUN isEqualToString:@"불용요청[OE]"]) bsnNo = @"0505";
-    else if([JOB_GUBUN isEqualToString:@"연식조회[OA]"] || [JOB_GUBUN isEqualToString:@"비품연식조회[OE]"]) bsnNo = @"0602";
-    else if([JOB_GUBUN isEqualToString:@"납품확인[OA]"] || [JOB_GUBUN isEqualToString:@"납품확인[OE]"]) bsnNo = @"0512";
-    else if([JOB_GUBUN isEqualToString:@"대여등록[OA]"] || [JOB_GUBUN isEqualToString:@"대여등록[OE]"]) bsnNo = @"0513";
-    else if([JOB_GUBUN isEqualToString:@"대여반납[OA]"] || [JOB_GUBUN isEqualToString:@"대여반납[OE]"]) bsnNo = @"0503";
-
-    isDataSaved = NO;
+    else if([JOB_GUBUN rangeOfString:@"연식조회"].location != NSNotFound){
+        bsnNo = @"0602";
+        locCodeView.hidden = YES;
+    }
+    else if([JOB_GUBUN rangeOfString:@"납품확인"].location != NSNotFound)
+        bsnNo = @"0512";
+    else if([JOB_GUBUN rangeOfString:@"대여등록"].location != NSNotFound)
+        bsnNo = @"0513";
+    else if([JOB_GUBUN rangeOfString:@"대여반납"].location != NSNotFound)
+        bsnNo = @"0503";
 }
 
 #pragma mark - KSCAN Notification
@@ -510,8 +380,8 @@
 - (void) processFailRequest:(requestOfKind)pid Message:(NSString*)message Status:(NSInteger)status
 {
     if(pid == REQUEST_BASE_MANAGEMENT || pid == REQUEST_BASE_ITEM_SEARCH){
-        locCode.text = strLocBarCode = @"";
-        facCode.text = strFacBarCode = @"";
+        locCode.text = @"";
+        facCode.text = @"";
         
         if (message.length){
             [self showMessage:message tag:-1 title1:@"닫기" title2:nil isError:YES];
